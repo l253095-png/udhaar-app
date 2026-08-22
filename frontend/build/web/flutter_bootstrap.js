63e8b86ffedf928003e1dacf4c8b11924c5e9583
Aziz Store <l253095@lhr.nu.edu.pk>
2026-08-22 19:46:10 +0500

    Rebuild frontend with Link By List feature

--- frontend/build/web/flutter_bootstrap.js
+++ frontend/build/web/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1165975641" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "963021540" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
