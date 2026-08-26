3a071b4983f54f30af49947c83a515f184f073c8
Aziz Store <l253095@lhr.nu.edu.pk>
2026-08-27 00:16:06 +0500

    Fix stray text in customer_detail_screen.dart

--- frontend/build/web/flutter_bootstrap.js
+++ frontend/build/web/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3015783460" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1162276013" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
